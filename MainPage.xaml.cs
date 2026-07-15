60db7b612c7419e0c9821af7ebf8c3ed68dba695
Thung <thunguyen.anh2209@gmail.com>
2026-07-15 19:45:49 +1000

    Update Call button

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,54 @@
 
 public partial class MainPage : ContentPage
 {
+    string result = string.Empty;
 
     public MainPage()
     {
         InitializeComponent();
-        
     }
-
-    public void OnTranslate()
+    
+    private void OnTranslate(object sender, EventArgs e)
     {
         string raw = PhoneNumberText.Text;
-        if (!string.IsNullOrWhiteSpace(raw))
+        if (!string.IsNullOrEmpty(raw))
         {
-            string result = PhonewordTranslator.ReceiveRaw(raw);
+            result = PhonewordTranslator.ReceiveRaw(raw);
+            
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + result;
         }
         else
         {
-            return;
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
         }
     }
     
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlertAsync(
+                "Dial a Number",
+                "Would you like to call " + result + "?",
+                "Yes",
+                "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported && !string.IsNullOrWhiteSpace(result))
+                {
+                    PhoneDialer.Default.Open(result);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlertAsync("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Xử lý các lỗi phát sinh khác ngoài dự kiến
+                await DisplayAlertAsync("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
 }
